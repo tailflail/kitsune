8aade6df42520724fd890c17e06dab7def6fff32
tailflail <jmccann.sco@gmail.com>
2022-12-15 16:26:57 +0000

    Add User model with Devise

---
+++ app/models/user.rb
@@ -0,0 +1,10 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  validates :username,
+            length: { minimum: 4, maximum: 16, message: "Username must be between 4 and 16 characters" },
+            format: { with: /\A[a-zA-Z][a-zA-Z0-9]+\z/, message: "%{value} is not a valid username" }
+end

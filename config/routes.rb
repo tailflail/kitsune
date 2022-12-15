caafbad65d1c7191c61df77b19b094ea5abe0dcc
tailflail <jmccann.sco@gmail.com>
2022-12-15 21:21:10 +0000

    Add home page with basic header and footer

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
+  root "home#index"
+  get "home", to: "home#index", as: "home"
   devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
 end

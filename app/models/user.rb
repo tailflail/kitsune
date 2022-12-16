fd70b524dccdc2e02d121e549b6ee38e2fdbf91b
tailflail <jmccann.sco@gmail.com>
2022-12-16 02:30:04 +0000

    Style forms for login and registration

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,6 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :username,
-            length: { minimum: 4, maximum: 16, message: "Username must be between 4 and 16 characters" },
-            format: { with: /\A[a-zA-Z][a-zA-Z0-9]+\z/, message: "%{value} is not a valid username" }
+            length: { minimum: 4, maximum: 16, message: "must be between 4 and 16 characters" },
+            format: { with: /\A[a-zA-Z][a-zA-Z0-9]+\z/, message: "%{value} format is invalid" }
 end

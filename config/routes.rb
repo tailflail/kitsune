085e752857a074d0d6b3094453b23dd789d8881e
tailflail <jmccann.sco@gmail.com>
2022-12-20 07:34:55 +0000

    Temporary fix: Style avatars to appear circular until cropping feature is implemented

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   root "home#index"
+
   get "home", to: "home#index", as: "home"
-  devise_for :users
+
+  devise_for :users, controllers: {
+    sessions: "users/sessions"
+  }
 end

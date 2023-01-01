747d58167ce325f8c8b978905138f61923aa891d
tailflail <jmccann.sco@gmail.com>
2023-01-01 10:02:14 +0000

    Fix: Raise 404 on user profile if user does not exist

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,8 @@
 class UsersController < ApplicationController
   def show
     @user = User.find_by_username(params[:username])
+    unless @user
+      raise ActionController::RoutingError.new('Not Found')
+    end
   end
 end
